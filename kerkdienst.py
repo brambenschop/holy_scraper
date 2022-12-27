8645a9e6bf6b9881954784bf6b0554789289b592
Lucaskembara <61460752+Lucaskembara@users.noreply.github.com>
2022-12-27 22:59:38 +0100

    Update kerkdienst.py

--- kerkdienst.py
+++ kerkdienst.py
@@ -1,38 +1,29 @@
-from selenium import webdriver
-
-from selenium.webdriver.chrome.service import Service
-
-from selenium.webdriver.support.ui import WebDriverWait
-
-from selenium.webdriver.support import expected_conditions as EC
-
-from bs4 import BeautifulSoup
-
-import codecs
-
 import re
-
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-
-val = input("https://kerkdienstgemist.nl/stations/1883-Hervormde-kerk-Kamerik/events")
+link = input("https://kerkdienstgemist.nl/stations/1883-Hervormde-kerk-Kamerik/events")
 
 wait = WebDriverWait(driver, 10)
-
-driver.get(val)
+driver.get(link)
 
 get_url = driver.current_url
-wait.until(EC.url_to_be(val))
+wait.until(EC.url_to_be(link))
 
-if get_url == val:
+if get_url == link:
     page_source = driver.page_source
 
-
 #nog ff aanpassen
-soup = BeautifulSoup(page_source,features=”html.parser”)
-keyword=input(“Enter a keyword to find instances of in the article:”)
+#Kan deze code weg?
+soup = BeautifulSoup(page_source,features="html.parser")
+keyword=input("Enter a keyword to find instances of in the article:")
 matches = soup.body.find_all(string=re.compile(keyword))
 
 len_match = len(matches)
