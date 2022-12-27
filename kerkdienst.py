ef58bb89ddfc3b03a51e2d7c9a73fe8642147454
brambenschop <94127899+brambenschop@users.noreply.github.com>
2022-12-27 22:19:14 +0100

    Update kerkdienst.py

--- kerkdienst.py
+++ kerkdienst.py
@@ -11,3 +11,31 @@ from bs4 import BeautifulSoup
 import codecs
 
 import re
+
+
+
+driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+
+
+val = input(“https://kerkdienstgemist.nl/stations/1883-Hervormde-kerk-Kamerik/events“)
+
+wait = WebDriverWait(driver, 10)
+
+driver.get(val)
+
+get_url = driver.current_url
+wait.until(EC.url_to_be(val))
+
+if get_url == val:
+    page_source = driver.page_source
+
+
+#nog ff aanpassen
+soup = BeautifulSoup(page_source,features=”html.parser”)
+keyword=input(“Enter a keyword to find instances of in the article:”)
+matches = soup.body.find_all(string=re.compile(keyword))
+
+len_match = len(matches)
+
+title = soup.title.text
