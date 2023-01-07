367b4b2df162b7f39756a207c5899655bb1aff52
brambenschop <94127899+brambenschop@users.noreply.github.com>
2023-01-07 17:42:14 +0100

    test

--- kerkdienst.py
+++ kerkdienst.py
@@ -1,31 +1,11 @@
-import re
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-link = input("https://kerkdienstgemist.nl/stations/1883-Hervormde-kerk-Kamerik/events")
-
-wait = WebDriverWait(driver, 10)
-driver.get(link)
-
-get_url = driver.current_url
-wait.until(EC.url_to_be(link))
-
-if get_url == link:
-    page_source = driver.page_source
-
-#nog ff aanpassen
-#Kan deze code weg?
-soup = BeautifulSoup(page_source,features="html.parser")
-keyword=input("Enter a keyword to find instances of in the article:")
-matches = soup.body.find_all(string=re.compile(keyword))
-
-len_match = len(matches)
-
-title = soup.title.text
+path = 'chromedriver'
+service = Service(executable_path=path)
+driver = webdriver.Chrome(service)
